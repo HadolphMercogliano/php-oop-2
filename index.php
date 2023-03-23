2532f5bf5470339e7adcdc382c411589d4497d65
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-23 17:07:12 +0100

    add classes

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+class Products {
+  public $id;
+  public $price;
+  public $name;
+  public $category
+}
 
+class Foods extends Products {
 
+}
+
+class Toys extends Products {
+
+}
+
+class PetBeds extends Products {
+
+}
+class Category {
+  public $category_name;
+}
 ?>
