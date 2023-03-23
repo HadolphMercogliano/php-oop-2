910ae2b54d4cac6ee77ad04b89ceffc94d1ce7be
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-23 18:16:46 +0100

    add food class prod

--- data/db.php
+++ data/db.php
@@ -1,11 +1,30 @@
 <?php
 $foods = [
   new Food("1",
-  "royal Canin",
+  "Royal Canin",
   "Puppy Mini",
   26.50,
   $dog_prod,
+  "https://www.petshop.co.uk/SCA%20Product%20Images/Royal-Canin-Mini-Puppy-Dry-Dog-Food-4kg_1.jpg?resizeid=5&resizeh=1200&resizew=1200",
   4.00,
   "bag"
+),
+new Food(
+  "2",
+"Chappie",
+"Original wet dog food",
+2.30,
+$dog_prod,
+"https://www.petshop.co.uk/SCA%20Product%20Images/Chappie-Original-Wet-Dog-Food-Cans-12-x-412g_1.jpg?resizeid=5&resizeh=1200&resizew=1200",
+0.412,
+"can"
+),
+new Food("3",
+"Royal Canin",
+"Sterilized", 
+25.00,$cat_prod,
+"https://www.petshop.co.uk/SCA%20Product%20Images/Royal-Canin-Sterilised-in-Jelly-Cat-Food-Pouches-12-x-85g_1.jpg?resizeid=5&resizeh=1200&resizew=1200",
+2.5,
+"bag"
 ),
 ];
--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
 
-class Products {
+class Product {
   public $id;
   public $name;
   public $price;
   public $brand;
   public $category;
+  public $img;
   
   public function __construct(
     int $_id,
     string $_brand,
     string $_name,
     float $_price,
-    $_category ) 
+    $_category,
+    string $_img 
+    ) 
     {
       $this->id = $_id;
       $this->brand = $_brand;
       $this->name = $_name;
       $this->price = $_price;
       $this->category = $_category;
+      $this->img = $_img;
+
     }
 
     // public function setName($name){
@@ -27,19 +32,80 @@ class Products {
     // }
 }
 
-class Foods extends Products {
+class Food extends Product {
   public $weight;
   public $package; 
+
+  public function __construct(
+    int $_id,
+    string $_brand,
+    string $_name,
+    float $_price,
+    $_category,
+    string $_img, 
+    float $_weight,
+    string $_package
+    ) 
+    {
+      $this->id = $_id;
+      $this->brand = $_brand;
+      $this->name = $_name;
+      $this->price = $_price;
+      $this->category = $_category;
+      $this->img = $_img;
+      $this->weight = $_weight;
+      $this->package = $_package;
+    }
 }
 
-class Toys extends Products {
+class Toy extends Product {
 public $type;
 public $material;
+
+public function __construct(
+    int $_id,
+    string $_brand,
+    string $_name,
+    float $_price,
+    $_category,
+    string $_img, 
+    string $_type,
+    string $_material
+    ) 
+    {
+      $this->id = $_id;
+      $this->brand = $_brand;
+      $this->name = $_name;
+      $this->price = $_price;
+      $this->category = $_category;
+      $this->img = $_img;
+      $this->type = $_type;
+      $this->material = $_material;
+    }
 }
 
-class PetBeds extends Products {
+class PetBed extends Product {
 public $material;
   
+public function __construct(
+    int $_id,
+    string $_brand,
+    string $_name,
+    float $_price,
+    $_category,
+    string $_img, 
+    string $_material
+     ) 
+    {
+      $this->id = $_id;
+      $this->brand = $_brand;
+      $this->name = $_name;
+      $this->price = $_price;
+      $this->category = $_category;
+      $this->img = $_img;
+      $this->material = $_material;
+
+    }
 }
 
 class Category {
@@ -51,4 +117,7 @@ class Category {
     $this->category_name = $_category_name;
   }
 }
+
+$dog_prod = new Category("dog");
+$cat_prod = new Category("cat")
 ?>
